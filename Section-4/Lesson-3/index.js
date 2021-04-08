0a559f2a4b5688caa2dc283aec0ad4d338ac4c45
zafergok <zfrgk96@gmail.com>
2021-04-09 02:45:57 +0300

    create es6 default modul example and package.json file

---
+++ Section-4/Lesson-3/index.js
@@ -0,0 +1,6 @@
+import xValue, { topla, cikarma } from "./myModule.js";
+
+console.log("topla : ", topla(3, 6));
+console.log("çıkarma : ", cikarma(23, 13));
+console.log("xValue : ", xValue(25));
+
---
+++ Section-4/Lesson-3/myModule.js
@@ -0,0 +1,11 @@
+export const topla = (a, b) => {
+  return a + b;
+};
+
+export const cikarma = (a, b) => {
+  return a - b;
+};
+
+export default (x) => {
+  return x * x;
+};
