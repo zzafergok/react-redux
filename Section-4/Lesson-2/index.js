0b0507fc2c63db49d9f7108c211bdb987dcf7846
zafergok <zfrgk96@gmail.com>
2021-04-09 02:45:34 +0300

    create es6 modul example and package.json file

---
+++ Section-4/Lesson-2/index.js
@@ -0,0 +1,7 @@
+import { topla } from "./myModule.js";
+import { cikarma } from "./myModule.js";
+
+
+console.log("topla : ", topla(3, 6));
+console.log("çıkarma : ", cikarma(23, 13));
+
---
+++ Section-4/Lesson-2/myModule.js
@@ -0,0 +1,7 @@
+export const topla = (a, b) => {
+  return a + b;
+};
+
+export const cikarma = (a, b) => {
+  return a - b;
+};
