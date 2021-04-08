df189d3050b4c59274733f5a68ab35a7a93c30a0
zafergok <zfrgk96@gmail.com>
2021-04-09 02:46:50 +0300

    create es6 'as' operator rename and package.json file

---
+++ Section-4/Lesson-4/index.js
@@ -0,0 +1,5 @@
+import xValue, { topla, cikarma as C } from "./myModule.js";
+
+console.log("topla : ", topla(3, 6));
+console.log("çıkarma : ", C(23, 13));
+console.log("xValue", xValue(32));
---
+++ Section-4/Lesson-4/myModule.js
@@ -0,0 +1,13 @@
+export const topla = (a, b) => {
+  return a + b;
+};
+
+export const cikarma = (a, b) => {
+  return a - b;
+};
+
+const xValue = (x) => {
+  return x * x;
+};
+
+export default xValue;
