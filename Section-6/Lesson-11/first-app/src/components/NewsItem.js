d2a40b7fd56c5b904582844e9a8f36ee06621abc
zafergok <zfrgk96@gmail.com>
2021-04-10 18:51:20 +0300

    created propTypes_shape_Tanımı

---
+++ Section-6/Lesson-11/first-app/src/components/NewsItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class NewsItem extends Component {
+  static propTypes = {
+    newsData: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    }).isRequired,
+  };
+  render() {
+    const { title, description } = this.props.newsData;
+    return (
+      <div> 
+        <h3>{title}</h3>
+        <h4>{description}</h4>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
