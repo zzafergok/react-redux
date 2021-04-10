c989d28cae9b442eaa85846d275ed7f9107e2dbb
zafergok <zfrgk96@gmail.com>
2021-04-10 18:58:21 +0300

    created propTypes_oneOfType_Tanımı

---
+++ Section-6/Lesson-9/first-app/src/components/News.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types"
+import NewsItem from "./NewsItem";
+
+class News extends Component {
+    static propTypes = {
+      news: PropTypes.oneOfTypes([
+        PropTypes.number,
+        PropTypes.array,
+        PropTypes.string,
+      ]),
+      name: PropTypes.string.isRequired
+    }
+  render() {
+    // const title = "Main Title";
+    // const description = "Main description";
+    // const news = [
+    //   {
+    //     title: "title1",
+    //     description: "description1",
+    //   },
+    //   {
+    //     title: "title2",
+    //     description: "description2",
+    //   },
+    //   {
+    //     title: "title3",
+    //     description: "description3",
+    //   },
+    // ];
+
+
+    const news = this.props.news.map((news, index) => (
+      <NewsItem key={index} title={news.title} description={news.description} />
+    ));
+    return (
+      <div>
+        {/* <NewsItem title={title} description={description} /> */}
+        {/* <NewsItem title={news[0].title} description={news[0].description} />
+        <NewsItem title={news[1].title} description={news[1].description} />
+        <NewsItem title={news[2].title} description={news[2].description} /> */}
+        {this.props.name} 
+        {news}
+      </div>
+    );
+  }
+}
+
+export default News;
