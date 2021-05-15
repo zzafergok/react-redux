e237ad0432c1e2b774f111112f2453601071b75a
zafergok <zfrgk96@gmail.com>
2021-05-15 12:10:03 +0300

    updated made simple

--- Section-16/mainfolder/src/App.js
+++ Section-16/mainfolder/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import { Link, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import { Route } from "react-router-dom";
+
 import MoviesPage from "./components/pages/MoviesPage";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Link to="movies"> MoviesPage </Link>
-        <Route path="/movies" component={MoviesPage} />
+        <Header />
+        <Container text>
+          <Route path="/movies" component={MoviesPage} />
+        </Container>
+        <Footer />
       </div>
     );
   }
