23a9cf98e5b02c6ad16b7934ba27607d573412d2
zafergok <zfrgk96@gmail.com>
2021-05-15 12:09:14 +0300

    updated store connection

--- Section-16/mainfolder/src/components/pages/MoviesPage.js
+++ Section-16/mainfolder/src/components/pages/MoviesPage.js
@@ -2,22 +2,40 @@ import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
+import { fetchMovies } from "../../actions/movies";
+
+import MoviesList from "./MoviesList";
+
 class MoviesPage extends React.Component {
   static propTypes = {
-    movies: PropTypes.array.isRequired,
+    movies: PropTypes.object.isRequired,
   };
 
+  componentDidMount() {
+    this.props.fetchMovies();
+  }
+
   render() {
+    console.log(this.props);
     return (
       <>
-        <h3>Merhaba Movies Page'desin</h3>
+        <div>
+          <h3>Movies Page</h3>
+          <MoviesList movies={this.props.movies} />
+        </div>
       </>
     );
   }
 }
 
 const mapStateToProps = ({ movies }) => {
-  return movies;
+  return {
+    movies,
+  };
 };
 
-export default connect(mapStateToProps)(MoviesPage);
+const mapDispatchToProps = {
+  fetchMovies,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MoviesPage);
